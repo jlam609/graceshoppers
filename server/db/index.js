c0b57d1f9687d52673660f3c3fe246f1b8ab330c
jlam609 <jlam609@gmail.com>
2020-07-25 20:29:22 -0400

    updated

--- server/db/index.js
+++ server/db/index.js
@@ -1,24 +1,23 @@
-const {db, models} = require('./models')
-const {Cart, Category, Order, Product, Session, User} = models
+const { db, models } = require("./models");
+const { Cart, Category, Order, Product, Session, User } = models;
 
 const seed = async (force = true) => {
-    try{
-        await db.sync({force})
-    }
-    catch(e){
-        throw new Error('seed unsuccessful', e)
-    }
-}
+  try {
+    await db.sync({ force });
+  } catch (e) {
+    throw new Error("seed unsuccessful", e);
+  }
+};
 
 module.exports = {
-    db,
-    seed,
-    models:{
-        Cart,
-        Category,
-        Order,
-        Product,
-        Session,
-        User
-    }
-}
+  db,
+  seed,
+  models: {
+    Cart,
+    Category,
+    Order,
+    Product,
+    Session,
+    User,
+  },
+};
