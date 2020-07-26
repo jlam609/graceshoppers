0aa99af479a641c75f5e5016fa9fbd994264177d
jlam609 <jlam609@gmail.com>
2020-07-25 22:57:25 -0400

    created some products, categories, and an admin user and created sync function to seed

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,189 @@
 const { db, models } = require("./models");
 const { Cart, Category, Order, Product, Session, User } = models;
 
+const sync = async() => {
+    const categoryList = [
+        {
+            name:'weapons',
+        }, 
+        {
+           name: 'armors'
+        }, 
+        {
+            name:'magic'
+        }, 
+        {
+            name:'items'
+        }
+    ]
+    const weaponList = [
+        {
+            name:'Master Sword',
+            description: 'Legend of Zelda series. Used by Link after being drawn from a stone altar. Said to be untouchable by evil.',
+            image:'https://cdn.vox-cdn.com/thumbor/Mc1qCq9ZPKGoxSx0CW3Jzr8sqb8=/0x0:1280x720/1400x1050/filters:focal(543x293:747x497):format(jpeg)/cdn.vox-cdn.com/uploads/chorus_image/image/53716843/2017030723220300_F1C11A22FAEE3B82F21B330E1B786A39.0.jpg',
+            categoryId:1,
+            price: 1000
+        },
+        {
+            name:'Excalibur', 
+            description:'The sword King Arthur got from the lady of the lake.',
+            image:'https://cdna.artstation.com/p/assets/images/images/006/541/116/large/angelo-gongora-excaliburwireframe.jpg?1499370386',
+            categoryId:1,
+            price: 1000
+        },
+        {
+            name:'Caladbolg',
+            description:'All who have brought the sun to this weapon have likely seen their fill of gulls \n -Final Fantasy All the Bravest description',
+            image:'https://templeofravens.files.wordpress.com/2016/10/ffx_weapon_-_caladbolg.png?w=605',
+            categoryId:1,
+            price: 1000
+        },
+        {
+            name:'Buster Sword',
+            description:'This sword is a symbol of my dreams... and my honor. No... it\'s more than that... \n Zack Fair',
+            image:'https://img1.cgtrader.com/items/881577/913f9ae6f9/buster-sword-final-fantasy-3d-model-obj-mtl-fbx-c4d-ma-mb-stl.jpg',
+            categoryId:1,
+            price:1000
+        },
+        {
+            name:'Blackfyre',
+            description: 'The Valyrian steel sword of Aegon I Targaryen. Carried by all Targaryen kings till Aegon IV who gave it to his bastard Daemon Blackfyre. \n Its whereabouts are currently unknown',
+            image:'https://vignette.wikia.nocookie.net/iron-throne-role-play/images/3/3b/Blackfyre.jpg/revision/latest?cb=20161113114804',
+            categoryId:1,
+            price:1000
+        },
+        {
+            name:'Masamune',
+            description: 'Sephiroth Sword is the most incredible, magnificent and most potent swords from the final fantasy series.',
+            image:'https://swordskingdom.com/media/catalog/product/m/a/masamune-sephiroth-sword-from-final-fantasy.jpg',
+            categoryId:1,
+            price:1000
+        },
+        {
+            name:'Apollo\'s Golden Bow',
+            description:'The bow can cause health or famine, although its main function is that of a regular bow, but with much greater power',
+            image:'https://vignette.wikia.nocookie.net/cookie-pantheon/images/8/8c/2235023_l.jpg/revision/latest/top-crop/width/720/height/900?cb=20190521203140',
+            categoryId:1,
+            price:1000
+        }
+    ]
+    const armorList = [
+        {
+            name:'Armour of Achilles',
+            description: 'created by Hephaestus and said to be impenetrable',
+            image:'https://i.pinimg.com/originals/f8/64/16/f86416f45bd423e5c60a7d03d13c2c46.jpg',
+            categoryId:2,
+            price:2000
+        },
+        {
+            name:'Mythril Armor',
+            description:'Made from mythril. It is described as resembling silver but being stronger and lighter than steel.',
+            image:'https://steamuserimages-a.akamaihd.net/ugc/576699532596996967/D91AECEAAD15C899B0AA5A0DE9CFF31F480CA008/',
+            categoryId:2,
+            price:2000
+        },
+        {
+            name:'Adamantine Armor',
+            description:'Made from adamantine. The hardest metal in the world',
+            image:'https://img2.finalfantasyxiv.com/accimg2/a8/71/a87185e92f648768a0196e0edfb92009b1cbab2d.jpg',
+            categoryId:2,
+            price:2000
+        },
+        {
+            name:'Garbage Bag',
+            description:'Nothing can penetrate almighty garbage',
+            image:'https://i.chzbgr.com/full/6210288128/hA81ADDCF/none-can-penetrate-my-bag-armor',
+            categoryId:2,
+            price: 5
+        }
+    ]
+    const spellList = [
+        {
+            name:'Dark Magic',
+            description:'Self-serving, stat-manipulating, leeching spells.',
+            image:'https://gamepedia.cursecdn.com/exvius_gamepedia_en/2/2f/Esper-Diabolos-3.png?version=ec615dc151ecceaa9316b146b3cef16d',
+            price:100,
+            categoryId:3
+        },
+        {
+            name:'White Magic',
+            description:'Curative spells that mend wounds.',
+            image:'https://i.pinimg.com/originals/89/1a/0c/891a0cc339757782e3bc6bfd953b69e6.jpg',
+            price:200,
+            categoryId:3
+        },
+        {
+            name:'Enhancing Magic',
+            description:'Beneficially affect allies with stat buffs.',
+            image:'https://img2.finalfantasyxiv.com/accimg/7a/65/7a658e53515157e2217daa61aa220f15a9b79d7e.jpg',
+            price:200,
+            categoryId:3
+        },
+        {
+            name:'Elemental Magic',
+            description:'Generic offensive damage-dealing spells.',
+            image:'https://vignette.wikia.nocookie.net/finalfantasy/images/8/88/Prime_Elements_Chart_FFXI_Art.png/revision/latest?cb=20120903224809',
+            price:100,
+            categoryId:3
+        },
+        {
+            name:'Summoning Magic',
+            description:'Elemental beasts evoked to aid combatants.',
+            image:'https://i.ytimg.com/vi/WS_xLSYNMHM/maxresdefault.jpg',
+            price:500,
+            categoryId:3
+        }
+    ]
+    const itemList = [
+        {
+            name:'Potion',
+            description:'Restores some lost health',
+            image:'https://sayonaraai.files.wordpress.com/2007/11/ffsp.jpg',
+            price:5,
+            categoryId:4
+        },
+        {
+            name:'Elixir',
+            description:'Completely restore health and mana',
+            image:'https://vignette.wikia.nocookie.net/finalfantasy/images/5/5c/Megalixir_FF7.png/revision/latest?cb=20100602160250',
+            price:20,
+            categoryId:4
+        },
+        {
+            name:'1UP',
+            description: 'Extra Life!',
+            image: 'https://www.kindpng.com/picc/m/600-6000351_transparent-mario-mushroom-png-super-mario-1up-mushroom.png',
+            price:100,
+            categoryId:4
+        },
+        {
+            name:'Antidote',
+            description:'A tonic that works through the skin, drawing out and neutralizing toxins within the body.',
+            image: 'https://www.antidote.info/accueil/_hero/fiole.png',
+            price:10,
+            categoryId:4
+        }
+    ]
+    await categoryList.map(category => Category.create(category))
+    await weaponList.map(weapon => Product.create(weapon))
+    await armorList.map(armor => Product.create(armor))
+    await spellList.map(spell => Product.create(spell))
+    await itemList.map(item => Product.create(item))
+    await User.create({
+        username:'admin@fullstack.com',
+        password:'password',
+        salt:'salt',
+        clearance:5
+    })
+}
+
 const seed = async (force = true) => {
   try {
     await db.sync({ force });
+    if (force){
+        await sync()
+    }
+    console.log('seed was successful')
   } catch (e) {
     throw new Error("seed unsuccessful", e);
   }
