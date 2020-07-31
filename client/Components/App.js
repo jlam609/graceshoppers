789a7226ac7d9effd44ac44d0c74d3bff839c10e
Jake Froehlich <jake.j.froehlich@gmail.com>
2020-07-30 20:14:47 -0400

    ran prettier and fixed linting

--- client/Components/App.js
+++ client/Components/App.js
@@ -1,24 +1,3 @@
-<<<<<<< HEAD
-import React, { Component, useEffect } from 'react';
-import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux'
-import { fetchCategories, fetchProducts } from '../store/actions.js'
-import WeaponsList from './WeaponsList.js'
-import ArmorList from './ArmorList.js'
-import SpellList from './SpellList.js'
-import ItemList from './ItemList.js'
-import Nav from './Nav'
-import HomePage from './Homepage'
-
-// const App = ({dispatch}) => {
-//     useEffect(() => {
-//         const getData = async() => {
-//             await dispatch(fetchProducts())
-//             await dispatch(fetchCategories())
-//         }
-//         getData()
-//     },[])
-=======
 /* eslint-disable react/no-unescaped-entities */
 import React, {Component} from "react";
 import {Route, Switch, Redirect} from "react-router-dom";
@@ -30,7 +9,6 @@ import SpellList from "./SpellList";
 import ItemList from "./ItemList";
 import Nav from "./Nav";
 import HomePage from "./Homepage";
->>>>>>> master
 
 class App extends Component {
   async componentDidMount() {
@@ -38,81 +16,25 @@ class App extends Component {
     await this.props.fetchCategories();
   }
 
-<<<<<<< HEAD
-    async componentDidMount() {
-        await this.props.fetchProducts();
-        await this.props.fetchCategories();
-    }
-    render() {
-        return (
-            <div
-                className='maindiv'>
-                {Nav()}
-                <Switch>
-                    <Route path='/weapons' component={WeaponsList} />
-                    <Route path='/armor' component={ArmorList} />
-                    <Route path='/magic' component={SpellList} />
-                    <Route path='/items' component={ItemList} />
-                    <Route path='/home' component={HomePage}></Route>
-                    <Redirect to='/home' />
-                </Switch>
-            </div>
-        )
-    }
-=======
   render() {
     return (
       <div>
         <h1>Tack's RNGeneral Store</h1>
         <Nav />
         <Switch>
-<<<<<<< HEAD
-             <Route path='/weapons' component = {WeaponsList}/> 
-             <Route path='/armor' component = {ArmorList}/>
-             <Route path='/magic' component = {SpellList} />
-             <Route path='/items' component = {ItemList} />
-            <Route path='/home' component={ HomePage }></Route>
-            <Redirect to='/home'/>
-=======
           <Route path="/weapons" component={WeaponsList} />
           <Route path="/armor" component={ArmorList} />
           <Route path="/magic" component={SpellList} />
           <Route path="/items" component={ItemList} />
           <Route path="/home" component={HomePage} />
           <Redirect to="/home" />
->>>>>>> dd167a5c3bfa814ff622d08a2e3a2f198a11fa23
         </Switch>
       </div>
     );
   }
->>>>>>> master
 }
 
 const mapStateToProps = (state) => {
-<<<<<<< HEAD
-    return {
-        products: state.products,
-        categories: state.categories,
-    }
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        fetchProducts: () => {
-            dispatch(fetchProducts())
-        },
-        fetchCategories: () => {
-            dispatch(fetchCategories())
-        }
-    };
-};
-// const mapDispatch = (dispatch) => {
-//     return{
-//         dispatch
-//     }
-// }
-export default connect(mapStateToProps, mapDispatchToProps)(App)
-=======
   return {
     products: state.products,
     categories: state.categories,
@@ -131,4 +53,3 @@ const mapDispatchToProps = (dispatch) => {
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
->>>>>>> master
--- client/Components/Nav.js
+++ client/Components/Nav.js
@@ -1,38 +1,18 @@
-<<<<<<< HEAD
-import React from 'react';
-import { Link } from 'react-router-dom';
-
-const Nav = () => {
-    return (
-        <div
-            className='nav' >
-            <img src='./Tacks_Sign.png'
-                className='logo'></img>
-            <Link to='/home'>Home</Link>
-            <Link to='/weapons'>Weapons</Link>
-            <Link to='/armor'>Armor</Link>
-            <Link to='/magic'>Magic</Link>
-            <Link to='/items'>Items</Link>
-            <Link to='/cart'>Cart</Link>
-        </div>
-    )
-}
-=======
 import React from "react";
 import {Link} from "react-router-dom";
 
 const Nav = () => {
   return (
-    <nav>
+    <div className="nav">
+      <img src="./Tacks_Sign.png" className="logo" />
       <Link to="/home">Home</Link>
       <Link to="/weapons">Weapons</Link>
       <Link to="/armor">Armor</Link>
       <Link to="/magic">Magic</Link>
       <Link to="/items">Items</Link>
       <Link to="/cart">Cart</Link>
-    </nav>
+    </div>
   );
 };
->>>>>>> master
 
 export default Nav;
