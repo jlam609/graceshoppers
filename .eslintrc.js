434ed91375ccd4a98d9de945b1be48b5608c08f1
jlam609 <jlam609@gmail.com>
2020-08-06 21:02:25 -0400

    fxed

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,6 +29,6 @@ module.exports = {
     "no-nested-ternary": 0,
     "no-unneeded-ternary": "off",
     "react/jsx-wrap-multilines": 0,
-    "import/order":"off"
+    "import/order": "off",
   },
 };
